7ab59f0afe7368b7ab0d51798ae644ed6e0068b9
JinjiGomez <jinjigomez@gmail.com>
2013-03-06 20:10:45 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 
 <head>
-  	<link rel="stylesheet" href="css/bootstrap.css" />
+		<link rel="stylesheet" href="css/bootstrap.css" />
 	
 
 		<link rel="stylesheet" href="css/font-awesome.css">
@@ -10,11 +10,12 @@
 		<div class="container">
 			
 			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span> </a>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span> 
+			</a>
 			 
-				<div class="nav-collapse collapse">
+			<div class="nav-collapse collapse">
 					<ul class="nav">
 						<li>
 							<a href="#" rel="tooltip" data-placement="top" title="Gothic" class="curtain-links">Gothic</a>
@@ -30,28 +31,26 @@
 						</li>
 					</ul>
 					<ul class="nav pull-right">
-					<li>
-						<a href="#" rel="tooltip" data-placement="top" title="Back to top"  class="curtain-links">Back to top</a>
-					</li>
-					<li class="dropdown">
-					  
-						<?php
-								if(isset($_SESSION['islogin']))
-								{
-									$user = $_SESSION['user'];
-									$fullname = $user['firstname']; ?>
-						<a class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i><?php echo $fullname; }?>
-						<b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<li><a href="addRecord.php"><i class="icon-upload-alt"></i> Add</a></li>
-							<li><a href="Recordlist.php"><i class="icon-pencil"></i> Edit</a></li>
-							<li><a href="Recordlist.php"><i class="icon-trash"></i> Delete</a></li>
-							<li><a href="Recordlist.php"><i class="icon-th"></i> View All</a></li>
-							<li class="divider"></li>
-							<li><a href="logout.php"><i class="icon-off"></i> Logout</a></li>
-						</ul>
-						
-					</li>
+						<li>
+							<a href="#" rel="tooltip" data-placement="top" title="Back to top"  class="curtain-links">Back to top</a>
+						</li>
+						<li class="dropdown">
+						  	<?php if(isset($_SESSION['islogin'])){
+										$user = $_SESSION['user'];
+										$fullname = $user['firstname']; ?>
+							<a class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i><?php echo $fullname; }?>
+								<b class="caret"></b>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="addRecord.php"><i class="icon-upload-alt"></i> Add</a></li>
+								<li><a href="Recordlist.php"><i class="icon-pencil"></i> Edit</a></li>
+								<li><a href="Recordlist.php"><i class="icon-trash"></i> Delete</a></li>
+								<li><a href="Recordlist.php"><i class="icon-th"></i> View All</a></li>
+								<li class="divider"></li>
+								<li><a href="logout.php"><i class="icon-off"></i> Logout</a></li>
+							</ul>
+							
+						</li>
 					</ul>
 			</div>
 		</div>
